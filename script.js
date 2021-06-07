5da17bf2add8edf727aea0375d1a270ba4591e04
George Trapp <george@MACBOOK-AIR.local>
2021-06-07 12:29:05 -0400

    tile active state

--- script.js
+++ script.js
@@ -10,26 +10,28 @@ const hints = document.getElementById("message");
 let diceSum = 0;
 let tileInt = 0;
 let tileSum = 0;
+let tilesActive;
 
 btnRoll.addEventListener("click", function () {
-  if (btnRoll.classList.contains("disable")) 
-    return;
+  if (btnRoll.classList.contains("disable")) return;
   //1. Generating a random dice roll
   const dice1 = Math.trunc(Math.random() * 6) + 1;
   const dice2 = Math.trunc(Math.random() * 6) + 1;
 
   //2. Display dice
   diceEl1.classList.remove("hidden");
-  diceEl2.classList.remove("hidden");WebGLUniformLocation
+  diceEl2.classList.remove("hidden");
   diceEl1.src = `//raw.githubusercontent.com/gtrapp/shut-the-box/main/dice-${dice1}.png`;
   diceEl2.src = `//raw.githubusercontent.com/gtrapp/shut-the-box/main/dice-${dice2}.png`;
 
   diceSum = dice1 + dice2;
-      console.log(diceSum);
+  console.log(diceSum);
+  
   // disable roll dice reset tile sum
-  //tileSum = 0;
-
+  tileSum = 0;
   disableDiceRoll();
+  enableTileSelect();
+  tilesActive = false;
 });
 
 const enableDiceRoll = function () {
@@ -38,43 +40,38 @@ const enableDiceRoll = function () {
 
   btnRoll.classList.remove("disable");
 };
+
 const disableDiceRoll = function () {
   btnRoll.classList.add("disable");
 };
 
-const enableTileSelect = function () {
-    tileEl.classList.add("tile--active");
- 
-  };
-  const disableTileSelect = function () {
-    tileEl.classList.add("tile--active");
-  };
-
-
 const resetTiles = function () {
   tileEl.forEach(function (btn) {
     btn.classList.remove("closed");
-    btn.classList.remove("tile--active");
+    btn.classList.remove("tile--selected");
   });
 };
 
 tileEl.forEach(function (btn) {
   btn.addEventListener("click", function () {
-    if (this.classList.contains("disable") || this.classList.contains("closed"))
+    if (
+      this.classList.contains("disable") ||
+      this.classList.contains("closed") ||
+      tilesActive
+    )
       return;
-    this.classList.add("tile--active");
 
+    this.classList.add("tile--selected");
     let tileNumber = this.textContent;
     tileInt = parseInt(tileNumber);
     tileSum += tileInt;
-
     tileCheck(tileSum);
   });
 });
 
 const closeSelectedTiles = function () {
   tileEl.forEach(function (btn) {
-    if (btn.classList.contains("tile--active")) {
+    if (btn.classList.contains("tile--selected")) {
       btn.classList.add("closed");
     }
   });
@@ -86,16 +83,29 @@ const tileCheck = function (tilesum) {
   if (tilesum == diceSum) {
     console.log("It's a match!");
     closeSelectedTiles();
+    tilesActive = true;
+    disableTileSelect();
     enableDiceRoll();
   } else if (tilesum > diceSum) {
     console.log("It's too high");
     for (let i = 0; i < tileEl.length; i++) {
-      tileEl[i].classList.remove("tile--active");
+      tileEl[i].classList.remove("tile--selected");
       tileSum = 0;
     }
   } else {
     console.log("It's too low");
   }
+};
+
+const enableTileSelect = function () {
+  tileEl.forEach(function (btn) {
+    btn.classList.add("tile--active");
+  });
+};
+const disableTileSelect = function () {
+  tileEl.forEach(function (btn) {
+    btn.classList.remove("tile--active");
+  });
 };
 
 // Starting conditions / New Game
@@ -104,6 +114,8 @@ const init = function () {
   diceEl2.classList.add("hidden");
   enableDiceRoll();
   resetTiles();
+  disableTileSelect();
+  tilesActive = true;
 };
 init();
 
